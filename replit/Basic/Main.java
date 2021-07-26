10c60aca617cda77ee7aa8a8548390bad8f2963a
B23-User <ikramazim@hotmail.com>
2021-07-26 13:08:58 -0400

    day35

--- replit/Basic/Main.java
+++ replit/Basic/Main.java
@@ -1,4 +1,6 @@
 package Basic;
+import sun.security.x509.InvalidityDateExtension;
+
 import java.util.*;
 
 
@@ -6,10 +8,33 @@ public class Main {
     public static void main(String[] args) {
 
 
-    }
+        Scanner scan = new Scanner(System.in);
+
+        String sentence = "Cat Dog";
+
+                int numberOfCats = 0;   // "Cat"
+                int numberOfDogs = 0; //"Dog"
+
+                for(int i = 0; i <= sentence.length()-3; i++){
+                    String each =    sentence.substring(i, i+3);
+                    if(each.equals("Cat")){
+                        numberOfCats++;
+                    }
+
+                    if(each.equals("Dog")){
+                        numberOfDogs++;
+                    }
+
+                }
 
 
-    }
+                // System.out.println (numberOfCats);
+                //System.out.println(numberOfDogs);
+
+                boolean equal =  numberOfCats == numberOfDogs;
+
+                System.out.println(equal);
 
 
-
+            }
+        }
--- src/day35_ArrayList/WarmUpTask1.java
+++ src/day35_ArrayList/WarmUpTask1.java
@@ -1,4 +1,12 @@
 package day35_ArrayList;
 
+import java.time.LocalDate;
+
 public class WarmUpTask1 {
+    public static void main(String[] args) {
+
+        LocalDate[] dates = new LocalDate[10];
+
+        System.out.println();
+    }
 }
